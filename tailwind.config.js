63362181ac800bad64097fa50fd9c2abd11709e6
jus2froit <juliandevienne@orange.fr>
2022-05-12 15:36:25 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['"Oleo Script Swash Caps"', 'cursive'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
